7163719e189434b1cbfcafa25e92c56ccd025fe4
Santosh Marigowda <santosharakere@gmail.com>
2018-08-26 20:56:25 +0100

    attack function

--- firstCourseProject/js/app.js
+++ firstCourseProject/js/app.js
@@ -10,6 +10,27 @@ new Vue({
       this.isGameRunning = true;
       this.monsterHealth = 100;
       this.playerHealth = 100;
+    },
+    attack: function() {
+      let max = 10;
+      let min = 3;
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.monsterHealth -= damage;
+
+      max = 15;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.playerHealth -= damage;
+
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
     }
   }
 })
