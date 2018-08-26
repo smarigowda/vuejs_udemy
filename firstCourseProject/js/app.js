6dc958b5da9682c4ed047d7d845343d2c8ae7b25
Santosh Marigowda <santosharakere@gmail.com>
2018-08-26 20:27:09 +0100

    implemented startGame method

--- firstCourseProject/js/app.js
+++ firstCourseProject/js/app.js
@@ -4,5 +4,12 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     isGameRunning: false
+  },
+  methods: {
+    startGame: function() {
+      this.isGameRunning = true;
+      this.monsterHealth = 100;
+      this.playerHealth = 100;
+    }
   }
 })
